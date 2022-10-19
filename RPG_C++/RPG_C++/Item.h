033b629790e4b0ce3de4b7d3b55023522758bc91
Gaëtan Peronnet <gaetan.peronnet@gmail.com>
2022-10-19 10:14:48 +0200

    update

--- RPG_C++/RPG_C++/Item.h
+++ RPG_C++/RPG_C++/Item.h
@@ -4,9 +4,10 @@ public:
 	string get_name();
 	int get_attack();
 	int get_defense();
+	string get_type();
 public:
 	string m_name;
-	string m_Type;
+	string m_type;
 	int m_defense;
 	int m_attack;
 };
